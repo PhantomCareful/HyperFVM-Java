01a0141d1619a215fe2775190ade26a38d8fb072
PhantomCareful <2314838130@qq.com>
2025-12-30 16:52:36 +0800

    Update version to 2.5.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.careful.HyperFVM"
         minSdk = 31
         targetSdk = 36
-        versionCode = 60
-        versionName = "2.4.9"
+        versionCode = 61
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
