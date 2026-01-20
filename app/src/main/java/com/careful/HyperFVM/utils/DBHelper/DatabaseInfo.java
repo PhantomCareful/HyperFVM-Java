95c3f50d0d471cc9f3f111fe59cfca02593bb69d
PhantomCareful <2314838130@qq.com>
2026-01-20 14:48:40 +0800

    Fix: Display bugs.

--- app/src/main/java/com/careful/HyperFVM/utils/DBHelper/DatabaseInfo.java
+++ app/src/main/java/com/careful/HyperFVM/utils/DBHelper/DatabaseInfo.java
@@ -78,6 +78,8 @@ public class DatabaseInfo {
      * 让猪年、鼠年、牛年、虎年、兔年生肖卡数据支持Markdown显示
      * DB_VERSION = 39
      * 让普通卡数据支持Markdown显示
+     * DB_VERSION = 40
+     * 修复几个显示问题
      */
-    public static final int DB_VERSION = 39;
+    public static final int DB_VERSION = 40;
 }
