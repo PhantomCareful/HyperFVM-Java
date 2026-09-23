babc7b6caf4f09e6f9a3310accd25638f1e2d9e4
Phantom Careful <2314838130@qq.com>
2026-09-23 16:46:58 +0800

    Update: Version 4.4.1(120).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ configure<ApplicationExtension> {
         applicationId = "com.careful.HyperFVM"
         minSdk = 31
         targetSdk = 37
-        versionCode = 119
+        versionCode = 120
         versionName = "4.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
